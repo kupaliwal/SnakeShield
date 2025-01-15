5fe6bacac4c33ad92379ccd3a82f1353ef546619
kpaliwal <104173471+kupaliwal@users.noreply.github.com>
2025-01-15 11:32:54 -0800

    adds command to add a banned id list

---
+++ SnakeShield bot.py
@@ -0,0 +1,66 @@
+import discord
+from discord import commands
+import random
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = discord.Client(intents=intents)
+
+#possible replys to blacklisted users
+possiblereply = ["shut up", "stop", "no", "nope", "nah", "stop being weird", "you arent allowed to message snake", "think before you speak", "back off my goat", "SOMEONE HELP","negative", "nuh-uh", "no way", "not happening", "forget it", "MODS HELP "  ]
+
+
+
+#these two are tests atm
+jaydnid = 947418695192436756
+smallsid = 429634654190960650
+bannedidlist = [jaydnid, smallsid]
+#lets you add more people to the banned list
+while True:
+    print("Banned ID list: ", bannedidlist)
+    print("Enter the ID of the user you want to ban from messaging snake. If you are done, type 'done'")
+    bannedid = input()
+    if bannedid == "done":
+        break
+    bannedidlist.append(int(bannedid))
+
+
+
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+
+#this part of the code is what makes the bot reply to messages
+@client.event 
+async def on_message(message):
+    if message.author == client.user:
+        return
+    message.content = message.content.lower()   
+    if "snake" in message.content and message.author.id in bannedidlist:
+        print("message found")
+        await message.channel.send(random.choice(possiblereply), reference=message)
+    elif "SNAKE" in message.content and message.author.id in bannedidlist:      
+        print("message found")
+        await message.channel.send(random.choice(possiblereply), reference=message)
+
+
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def banid(ctx, arg):
+    arg.append[bannedidlist]
+    await ctx.send("Banned ID ")
+
+
+
+#api key 
+apikey = input("Enter your bot token: ")
+client.run(apikey)
